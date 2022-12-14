ccda1323ef469a19a5a5ee4060e7d6ea73bf847c
jorgengj <jorgen.jarva@hotmail.com>
2022-12-14 02:18:53 +0200

    Add Week7 code

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Enter the operation (+/-):");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a:");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int lastNum = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+    case '+':
+        Addition(firstNum, lastNum);
+        break;
+
+    case '-':
+        Substraction(firstNum, lastNum);
+        break;
+    default:
+        Console.WriteLine("Invalid operator");
+        break;
+}
+
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Substraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
